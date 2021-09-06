2b69dfa45b2766754f2af0af93819d352d11c0c5
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-07 02:15:44 +0300

    Add a backdrop hook component

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import MainHeader from './MainHeader';
 import './MainNavigation.css';
@@ -6,15 +6,24 @@ import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
 
 const MainNavigation = props => {
+  const [DrawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => {
+    setDrawerIsOpen(true)
+  }
   return (
     <>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      { 
+        DrawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+        )
+      }
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
           <span />
           <span />
           <span />
