a2b468205909ca4972f674e9cc1556bf59af2c31
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-07 10:16:04 +0300

    Create a user places component

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Tanzania Ports Authority Tower',
+    description: 'One of the tallest buildings in Tanzania',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Tanzania_Port_Authority_Headquarters_%28cropped%29.jpg/800px-Tanzania_Port_Authority_Headquarters_%28cropped%29.jpg',
+    address: 'Dar es Salaam',
+    location: {
+      lat: -6.822004,
+      lng: 39.2841322
+    },
+    creator: 'u1'
+  },
+
+  {
+    id: 'p2',
+    title: 'Tanzania Ports Authority Tower',
+    description: 'One of the tallest buildings in Tanzania',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Tanzania_Port_Authority_Headquarters_%28cropped%29.jpg/800px-Tanzania_Port_Authority_Headquarters_%28cropped%29.jpg',
+    address: 'Dar es Salaam',
+    location: {
+      lat: -6.822004,
+      lng: 39.2841322
+    },
+    creator: 'u2'
+  }
+]
 
 const UserPlaces = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+  return 
+    <PlaceList items={DUMMY_PLACES} /> 
 }
 
 export default UserPlaces;
